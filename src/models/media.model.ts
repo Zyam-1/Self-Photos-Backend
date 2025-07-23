f297af8038fe4bc23c77f9d27c68d94c6ddfdc0e
Zyam Maqsood <zyamm7@gmail.com>
2025-07-24 03:56:18 +0500

    fixed some errors

--- src/models/media.model.ts
+++ src/models/media.model.ts
@@ -12,7 +12,6 @@ const mediaSchema = new mongoose.Schema<IMedia>({
     userID: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     fileName: {type: String, required: true},
     filePath: {type: String, required: true},
-<<<<<<< HEAD
     uploadedAt: {type: Date, default: Date.now()}
 })
 
@@ -20,7 +19,3 @@ const mediaSchema = new mongoose.Schema<IMedia>({
 const mediaModel = mongoose.model<IMedia>("Media", mediaSchema);
 
 export default mediaModel;
-=======
-    uploadedAt: {type: Date, default: new Date()}
-})
->>>>>>> de311b36b6586215a1ae701940efc7588b2203af
