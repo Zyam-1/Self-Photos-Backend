d49dc088e25d72d7dabde5f26187b60a00fd0b70
Zyam Maqsood <zyammaqsood7@gmail.com>
2025-08-03 04:01:10 +0500

    Implement secure media upload and retrieval with user-only access and pagination

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,5 +1,8 @@
 import {Request, Response} from "express";
 import mediaModel from "../models/media.model";
+import path from "path";
+
+const BASE_URL = process.env.BASE_URL;
 
 
 export const mediaUpload = async (req: Request, res: Response) => {
@@ -28,6 +31,54 @@ export const mediaUpload = async (req: Request, res: Response) => {
         });
     } catch (error) {
         console.error("Error in mediaUpload: ", error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+
+export const getMediaData = async (req: Request, res: Response) => {
+    //pagination
+    const page = Number(req.query.page) || 1;
+    const perPage = Number(req.query.perPage) || 10;
+    const skip = (page - 1) * perPage;
+
+    try {
+        const _id = req.user?._id;
+        const images = await mediaModel.find({_id}).sort({uploadedAt: -1 }).skip(skip).limit(perPage);
+
+        if(!images){
+            return res.status(404).json({message: "No Images Found"});
+        }
+
+        const imageURIs = images.map((image) => ({
+            id: image._id,
+            url: `${BASE_URL}/media/${image.fileName}`,
+            createdAt: image.uploadedAt
+        }));
+
+        return res.json({images: imageURIs});        
+    } catch (error) {
+        console.error("Error in getMediaData: ", error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+
+export const getMediaFile = async (req: Request, res: Response) => {
+    try {
+        const _id = req.user?._id;
+        const fileName = req.params.filename;
+        const image = await mediaModel.findOne({fileName});
+        if(!image) {
+            return res.status(404).json({message: "Image Not Found"});
+        }
+        if(image.userID !== _id){
+            return res.status(403).json({message: "Not Authorised"});
+        }
+        const filePath = path.join(__dirname, "..", "uploads", fileName);
+        return res.sendFile(filePath);        
+    } catch (error) {
+        console.error("Error in getMediaFiles: ", error);
         return res.status(500).json({message: "Internal Server Error"});
     }
 }
--- src/routes/media.route.ts
+++ src/routes/media.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { mediaUpload } from "../controllers/media.controller";
+import { getMediaData, getMediaFile, mediaUpload } from "../controllers/media.controller";
 import { upload } from "../config/multer";
 import { protect } from "../middleware/auth.middleware";
 
@@ -7,6 +7,12 @@ import { protect } from "../middleware/auth.middleware";
 const mediaRouter = express.Router();
 
 mediaRouter.post("/upload", protect, upload.array("files", 10), mediaUpload);
+//this route would get the media and urls
+mediaRouter.get("/media", protect, getMediaData);
+//this url will hit the url that is fetched in the /media endpoint
+mediaRouter.get("/media/:filename", protect, getMediaFile);
+
+
 
 
 export default mediaRouter;
